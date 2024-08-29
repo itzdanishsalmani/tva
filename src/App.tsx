9919d1d55a667183b68cfe14f849a4b692a86b6b
Danish Salmani <salmanidanish488@gmail.com>
2024-08-29 23:34:33 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+function App() {
+
+  return (
+    <>
+    <div className="w-screen h-screen bg-cover"
+    style={{
+      backgroundImage: `url('/hhh.jpg')`,}} >
+        <div className="w-screen h-screen border-4 border-orange-500 shadow-2xl shadow-orange-500	">
+          <div className="flex justify-center items-center h-screen ">
+          <div className=" border text-orange-500  border-orange-500 bg-black p-4 text-7xl font-bold">
+            FOR ALL TIMES. ALWAYS.
+          </div>
+          </div>
+        </div>
+    </div>
+
+    <div>
+      
+    </div>
+    </>
+  )
+}
+
+export default App

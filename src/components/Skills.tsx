bc3dbbc0a366a3a72d96714c3491132d60fbeee8
Danish Salmani <salmanidanish488@gmail.com>
2024-08-31 10:56:19 +0530

    Video added

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,7 +1,8 @@
 export const Skills = () => {
     return (
-        <div className="w-screen">
-            <img src="respect-deadline.gif" alt=""  />
-        </div>
+        <div className="w-screen bg-black h-screen flex justify-center pt-12">
+<video src="final.mp4" autoPlay muted loop className=" h-48" /> 
+<video src="final.mp4" autoPlay muted loop className=" h-48" /> 
+     </div>
     )
 }

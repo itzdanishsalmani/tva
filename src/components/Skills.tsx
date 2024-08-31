c6b6bb329ef385ae3867de373985ad32731cacee
Danish Salmani <salmanidanish488@gmail.com>
2024-08-31 20:48:07 +0530

    Skills page added

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,8 +1,93 @@
 export const Skills = () => {
     return (
-        <div className="w-screen bg-black h-screen flex justify-center pt-12">
-<video src="final.mp4" autoPlay muted loop className=" h-48" /> 
-<video src="final.mp4" autoPlay muted loop className=" h-48" /> 
+        <div className="relative w-screen h-screen p-4 bg-black">
+            <div className="custom-div">
+                <div className=" bg-orange-700 m-4 p-2 text-4xl text-center font-bold font-mono">RESPECT THE DEADLINE</div>
+        <div className="w-full h-fit flex justify-center pt-12">
+            <video src="final.mp4" autoPlay muted loop className=" h-48" /> 
+            <video src="final.mp4" autoPlay muted loop className=" h-48" /> 
+        </div>
+        <div className="bg-orange-700 h-2 mt-2">
+        </div>
+        <div className="bg-orange-700 h-8 mt-2 p-2"></div>
+        <div className=""></div>
+        <div className="border-t-2 border-orange-400 mt-2 text-orange-400 grid grid-cols-9 p-2 font-semibold">
+            <div>
+                PROJECT#
+                <div className="mt-4">
+                    <div>46412321=091</div>
+                    <div>46409821=475</div>
+                    <div>46415412=590</div>
+                    <div>46417630=767</div>
+                    <div>46418750=532</div>
+                </div>
+            </div>
+
+            <div>
+                TEAM
+                <div className="mt-4">
+                    <div>934</div>
+                    <div>19999</div>
+                    <div>92</div>
+                    <div>17</div>
+                    <div>9</div>
+                </div>
+            </div>
+            
+            <div>
+                DEADLINE
+                <div className="mt-4">
+                    <div>30.10.2023</div>
+                    <div>10.11.2023</div>
+                    <div>17.07.2027</div>
+                    <div>18.15.1413</div>
+                    <div>15.11.9125</div>
+                </div>
+            </div>
+
+            <div>
+                LANGUAGES
+                <div className="mt-4">
+                    <div>TYPESCRIPT</div>
+                    <div>JAVASCRIPT</div>
+                </div>
+            </div>
+
+            <div>
+                FRAMEWORKS
+                <div className="mt-4">
+                    <div>NEXT JS</div>
+                    <div>REACT JS</div>
+                    <div>NODE JS</div>
+                </div>
+            </div>
+
+            <div>
+                DATABASES
+                <div className="mt-4">
+                    <div>MONGODB</div>
+                    <div>POSTGRESQL</div>
+                </div>
+            </div>
+
+            <div>
+                OTHERS
+                <div className="mt-4">
+                    <div></div>
+                    <div>EXPRESS JS</div>
+                    <div>PRISMA</div>
+                    <div>AWS</div> 
+                    <div>DOCKER</div>
+                    <div>TAILWIND CSS</div>
+                </div>
+            </div>
+
+            <div className="img-transparent">
+                <img src="mm2.webp" alt="" className="w-48" />
+            </div>
+            
+        </div>
+        </div>
      </div>
     )
 }
